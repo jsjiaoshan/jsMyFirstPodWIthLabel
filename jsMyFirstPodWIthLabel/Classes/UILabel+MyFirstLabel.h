fa2712aa1a59c2629bc2f143fe223650bb87ef4b
jiaoshan <2977530037@qq.com>
2019-12-17 17:04:33 +0800

    jiaoshan

--- jsMyFirstPodWIthLabel/Classes/UILabel+MyFirstLabel.h
+++ jsMyFirstPodWIthLabel/Classes/UILabel+MyFirstLabel.h
@@ -5,15 +5,18 @@
 //  Created by 焦珊 on 2019/12/17.
 //
 
-#import <AppKit/AppKit.h>
+
 
 
 #import <UIKit/UIKit.h>
-
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UILabel (MyFirstLabel)
-
++(UILabel *_Nonnull)labelWithFrame:(CGRect)frame
+     andText:(NSString *_Nonnull)text
+     andFont:(UIFont *_Nonnull)font
+     andTextColor:(UIColor *_Nonnull)textColor
+andAlignment:(NSTextAlignment)alignment;
 @end
 
 NS_ASSUME_NONNULL_END
--- jsMyFirstPodWIthLabel/Classes/UILabel+MyFirstLabel.m
+++ jsMyFirstPodWIthLabel/Classes/UILabel+MyFirstLabel.m
@@ -6,10 +6,28 @@
 //
 
 #import "UILabel+MyFirstLabel.h"
-
 #import <AppKit/AppKit.h>
 
 
-@implementation UILabel (MyFirstLabel)
 
+@implementation UILabel (MyFirstLabel)
++(UILabel *_Nonnull)labelWithFrame:(CGRect)frame
+     andText:(NSString *_Nonnull)text
+     andFont:(UIFont *_Nonnull)font
+     andTextColor:(UIColor *_Nonnull)textColor
+                      andAlignment:(NSTextAlignment)alignment{
+    UILabel *label = [[UILabel alloc]initWithFrame:frame];
+    if(font){
+        [label setFont:font];
+    }
+    if(text){
+        [label setText:text];
+    }
+    if(textColor){
+        [label setTextColor:textColor];
+    }
+        [label setTextAlignment:alignment];
+    return label;
+    
+}
 @end
